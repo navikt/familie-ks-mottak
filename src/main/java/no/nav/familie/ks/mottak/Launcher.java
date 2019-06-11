41a66edb1ca2f33eb620abe94491a1d8fda3ee71
Martine Enger <marenger@student.matnat.uio.no>
2019-06-11 13:31:33 +0200

    Lagt til shutdownhook for nedetidsfri deploy. Konfigurerer jetty, legger til http.proxy (usikker på hva den brukes til men den ligger i de andre appene)

--- src/main/java/no/nav/familie/ks/mottak/Launcher.java
+++ src/main/java/no/nav/familie/ks/mottak/Launcher.java
@@ -1,13 +1,20 @@
 package no.nav.familie.ks.mottak;
 
+import no.nav.familie.ks.mottak.config.ApplicationConfig;
+import no.nav.familie.ks.mottak.config.DelayedShutdownHook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class Launcher {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Launcher.class, args);
+    public static void main(String... args) {
+        SpringApplication app = new SpringApplication(ApplicationConfig.class);
+        app.setRegisterShutdownHook(false);
+        ConfigurableApplicationContext applicationContext = app.run(args);
+        Runtime.getRuntime().addShutdownHook(new DelayedShutdownHook(applicationContext));
     }
 
 }
---
+++ src/main/java/no/nav/familie/ks/mottak/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package no.nav.familie.ks.mottak.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan({ "no.nav.familie.ks.mottak" })
+public class ApplicationConfig {
+
+    @Bean
+    ServletWebServerFactory servletWebServerFactory() {
+
+        JettyServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+
+        serverFactory.setPort(8080);
+
+        return serverFactory;
+    }
+}
