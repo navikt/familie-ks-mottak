abddb643e132a544b64aabadea43804668aea635
Henning Håkonsen <henning.hakonsen@nav.no>
2019-07-05 11:58:13 +0200

    Implementert sts client og flyt for søknad. (#4)
    * Implementert sts client. Lagt til flyt fra kontantstotte-api -> hente sts token -> kalle ks-mottak.
    
    * Endre URL mot ks-sak.
    
    * Sett unprotected på motta søknad endepunkt for test.
    
    * Forbedre flyt i accessToken cache.
    
    * Lagt til sts URL for preprod og prod. Debug logging av token fra sts.
    
    * Oppdatert sak url med absolutt path.
    
    * Ryddet opp i URLer i application yamler.
    
    * Setter spring profil til preprod ved preprod deploy.
    
    * Gå over til localtime for å lettere trekke fra minutter på expiration time. Fjerner debug logging.

--- src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
+++ src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
@@ -1,20 +1,57 @@
 package no.nav.familie.ks.mottak.api;
 
+import no.nav.familie.ks.mottak.sts.StsRestClient;
 import no.nav.security.oidc.api.ProtectedWithClaims;
 import no.nav.security.oidc.api.Unprotected;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 
 @RestController
 @RequestMapping("/api")
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
+@ProtectedWithClaims(issuer = "intern")
 public class MottakController {
 
+    private HttpClient client;
+    private URI sakServiceUri;
+    private StsRestClient stsRestClient;
+
+    @Autowired
+    public MottakController(@Value("${SOKNAD_KONTANTSTOTTE_SAK_API_URL}") URI sakServiceUri, @Autowired StsRestClient stsRestClient) {
+        this.client = HttpClient.newHttpClient();
+        this.sakServiceUri = URI.create(sakServiceUri + "/mottak/dokument");
+        this.stsRestClient = stsRestClient;
+    }
 
     @PostMapping(value = "/soknad", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String mottaSoknad(@RequestBody String soknad) {
-        return soknad;
+    @Unprotected
+    public ResponseEntity mottaSoknad(@RequestBody String soknad) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(sakServiceUri)
+                .POST(HttpRequest.BodyPublishers.ofString(soknad))
+                .header("Authorization", "Bearer " + stsRestClient.getSystemOIDCToken())
+                .header("Content-Type", "application/json")
+                .timeout(Duration.ofMinutes(2))
+                .build();
+
+        try {
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping("/ping")
