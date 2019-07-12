3c1c307e44fe89145970878dcd80b5450a31bea4
Henning Håkonsen <henninghaakonsen@me.com>
2019-07-12 09:47:25 +0200

    Fjernet profile condition på flyway bean creation.

--- src/main/java/no/nav/familie/ks/mottak/config/FlywayConfiguration.java
+++ src/main/java/no/nav/familie/ks/mottak/config/FlywayConfiguration.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ConditionalOnProperty("spring.flyway.enabled")
 public class FlywayConfiguration {
 
     @Bean
-    @Profile("!dev")
     public FlywayConfigurationCustomizer flywayConfig(@Value("${spring.cloud.vault.database.role}") String role) {
         return c -> c.initSql(String.format("SET ROLE \"%s\"", role));
     }
