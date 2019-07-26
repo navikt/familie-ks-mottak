ab36b91e91454b02617db2cd3a005b59db1f0a06
Martine Enger <marenger@student.matnat.uio.no>
2019-07-26 10:48:36 +0200

    Logg token fra sts

--- src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
+++ src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
@@ -2,6 +2,8 @@ package no.nav.familie.ks.mottak.api;
 
 import no.nav.familie.ks.mottak.sts.StsRestClient;
 import no.nav.security.oidc.api.ProtectedWithClaims;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -26,6 +28,7 @@ public class MottakController {
     private HttpClient client;
     private URI sakServiceUri;
     private StsRestClient stsRestClient;
+    private static final Logger LOG = LoggerFactory.getLogger(MottakController.class);
 
     @Autowired
     public MottakController(@Value("${SOKNAD_KONTANTSTOTTE_SAK_API_URL}") URI sakServiceUri, @Autowired StsRestClient stsRestClient) {
@@ -36,10 +39,13 @@ public class MottakController {
 
     @PostMapping(value = "/soknad", consumes = MediaType.APPLICATION_JSON_VALUE)
     public HttpResponse mottaSoknad(@RequestBody String soknad) throws IOException, InterruptedException {
+        String STSToken = stsRestClient.getSystemOIDCToken();
+        LOG.info("STS-token: {}", STSToken);
+
         HttpRequest request = HttpRequest.newBuilder()
             .uri(sakServiceUri)
             .POST(HttpRequest.BodyPublishers.ofString(soknad))
-            .header("Authorization", "Bearer " + stsRestClient.getSystemOIDCToken())
+            .header("Authorization", "Bearer " + STSToken)
             .header("Content-Type", "application/json")
             .timeout(Duration.ofMinutes(2))
             .build();
