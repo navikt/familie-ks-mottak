b9543b3dc5fc97cffe94bbb4ee8e102d18ead845
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-08-02 14:30:04 +0200

    Merge master

--- src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
+++ src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
@@ -1,6 +1,5 @@
 package no.nav.familie.ks.mottak.api;
 
-import no.nav.familie.ks.mottak.config.ApplicationConfig;
 import no.nav.familie.ks.mottak.sts.StsRestClient;
 import no.nav.security.oidc.OIDCConstants;
 import no.nav.security.oidc.api.ProtectedWithClaims;
@@ -9,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
-
 @RestController
 @RequestMapping("/api")
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = {"acr=Level4"})
@@ -34,18 +34,17 @@ public class MottakController {
     private URI sakServiceUri;
     private StsRestClient stsRestClient;
     private static final String SELVBETJENING = "selvbetjening";
-    private static final Logger log = LoggerFactory.getLogger(MottakController.class);
-
+    private static final Logger LOG = LoggerFactory.getLogger(MottakController.class);
 
     @Autowired
-    public MottakController(@Value("${SOKNAD_KONTANTSTOTTE_SAK_API_URL}") URI sakServiceUri, @Autowired StsRestClient stsRestClient) {
+    public MottakController(@Value("${FAMILIE_KS_SAK_API_URL}") URI sakServiceUri, @Autowired StsRestClient stsRestClient) {
         this.client = HttpClient.newHttpClient();
         this.sakServiceUri = URI.create(sakServiceUri + "/mottak/dokument");
         this.stsRestClient = stsRestClient;
     }
 
     @PostMapping(value = "/soknad", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public HttpResponse mottaSoknad(@RequestBody String soknad) throws IOException, InterruptedException {
+    public ResponseEntity mottaSoknad(@RequestBody String soknad) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(sakServiceUri)
             .POST(HttpRequest.BodyPublishers.ofString(soknad))
@@ -54,8 +53,14 @@ public class MottakController {
             .header("Nav-Personident", hentFnrFraToken())
             .timeout(Duration.ofMinutes(2))
             .build();
-        log.info("Sender søknad til " + sakServiceUri);
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+        LOG.info("Sender søknad til " + sakServiceUri);
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != HttpStatus.OK.value()) {
+            LOG.warn("Innsending til sak feilet. Responskode: {}. Feilmelding: {}", response.statusCode(), response.body());
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     private static String hentFnrFraToken() {
