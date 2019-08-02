ce6f01d5d5086da6c0010963a4ed51a3d50fbb92
Martine Enger <martine.enger@nav.no>
2019-08-02 10:34:33 +0200

    Litt div 🤓 (#8)
    Sett returtype til ResponseEntity i motta søknad-endepkt slik at det går an å deserialisere responsen fra sak. Sett opp litt softere caching av token da vi noen ganger ser at cachet token er utgått og ikke blir akseptert av serveren

--- src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
+++ src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
@@ -2,9 +2,13 @@ package no.nav.familie.ks.mottak.api;
 
 import no.nav.familie.ks.mottak.sts.StsRestClient;
 import no.nav.security.oidc.api.ProtectedWithClaims;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,24 +30,30 @@ public class MottakController {
     private HttpClient client;
     private URI sakServiceUri;
     private StsRestClient stsRestClient;
+    private static final Logger LOG = LoggerFactory.getLogger(MottakController.class);
 
     @Autowired
-    public MottakController(@Value("${SOKNAD_KONTANTSTOTTE_SAK_API_URL}") URI sakServiceUri, @Autowired StsRestClient stsRestClient) {
+    public MottakController(@Value("${FAMILIE_KS_SAK_API_URL}") URI sakServiceUri, @Autowired StsRestClient stsRestClient) {
         this.client = HttpClient.newHttpClient();
         this.sakServiceUri = URI.create(sakServiceUri + "/mottak/dokument");
         this.stsRestClient = stsRestClient;
     }
 
     @PostMapping(value = "/soknad", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public HttpResponse mottaSoknad(@RequestBody String soknad) throws IOException, InterruptedException {
+    public ResponseEntity mottaSoknad(@RequestBody String soknad) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(sakServiceUri)
-            .POST(HttpRequest.BodyPublishers.ofString(soknad))
-            .header("Authorization", "Bearer " + stsRestClient.getSystemOIDCToken())
-            .header("Content-Type", "application/json")
-            .timeout(Duration.ofMinutes(2))
-            .build();
+                .uri(sakServiceUri)
+                .POST(HttpRequest.BodyPublishers.ofString(soknad))
+                .header("Authorization", "Bearer " + stsRestClient.getSystemOIDCToken())
+                .header("Content-Type", "application/json")
+                .timeout(Duration.ofMinutes(2))
+                .build();
 
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != HttpStatus.OK.value()) {
+            LOG.warn("Innsending til sak feilet. Responskode: {}. Feilmelding: {}", response.statusCode(), response.body());
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
