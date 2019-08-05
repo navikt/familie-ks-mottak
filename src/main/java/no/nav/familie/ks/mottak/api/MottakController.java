70167d61002c4b0f40fc37cca4b4c79d1131b4d4
Nina Kjekstad <ninakjek@users.noreply.github.com>
2019-08-05 15:27:25 +0200

    Legg personident i header + oppdatert port

--- src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
+++ src/main/java/no/nav/familie/ks/mottak/api/MottakController.java
@@ -1,7 +1,9 @@
 package no.nav.familie.ks.mottak.api;
 
 import no.nav.familie.ks.mottak.sts.StsRestClient;
+import no.nav.security.oidc.OIDCConstants;
 import no.nav.security.oidc.api.ProtectedWithClaims;
+import no.nav.security.oidc.context.OIDCValidationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -20,7 +24,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-
 
 @RestController
 @RequestMapping("/api")
@@ -30,6 +33,8 @@ public class MottakController {
     private HttpClient client;
     private URI sakServiceUri;
     private StsRestClient stsRestClient;
+
+    private static final String SELVBETJENING = "selvbetjening";
     private static final Logger LOG = LoggerFactory.getLogger(MottakController.class);
 
     @Autowired
@@ -42,12 +47,14 @@ public class MottakController {
     @PostMapping(value = "/soknad", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity mottaSoknad(@RequestBody String soknad) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(sakServiceUri)
-                .POST(HttpRequest.BodyPublishers.ofString(soknad))
-                .header("Authorization", "Bearer " + stsRestClient.getSystemOIDCToken())
-                .header("Content-Type", "application/json")
-                .timeout(Duration.ofMinutes(2))
-                .build();
+            .uri(sakServiceUri)
+            .POST(HttpRequest.BodyPublishers.ofString(soknad))
+            .header("Authorization", "Bearer " + stsRestClient.getSystemOIDCToken())
+            .header("Content-Type", "application/json")
+            .header("Nav-Personident", hentFnrFraToken())
+            .timeout(Duration.ofMinutes(2))
+            .build();
+        LOG.info("Sender søknad til " + sakServiceUri);
 
         HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() != HttpStatus.OK.value()) {
@@ -55,5 +62,11 @@ public class MottakController {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    private static String hentFnrFraToken() {
+        OIDCValidationContext context = (OIDCValidationContext) RequestContextHolder.currentRequestAttributes().getAttribute(OIDCConstants.OIDC_VALIDATION_CONTEXT, RequestAttributes.SCOPE_REQUEST);
+        context = context != null ? context : new OIDCValidationContext();
+        return context.getClaims(SELVBETJENING).getClaimSet().getSubject();
     }
 }
