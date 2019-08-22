431428d9f718c4e6656c62a6e693f9ad0f275bf2
Marius Glittum <marius.glittum@nav.no>
2019-08-22 08:48:12 +0200

    Flytter endringen i egen migrering

--- src/main/resources/db/migration/V1__mottak.sql
+++ src/main/resources/db/migration/V1__mottak.sql
@@ -1,23 +1,3 @@
-DROP TABLE IF EXISTS MOTTAK;
-
-CREATE TABLE HENVENDELSE
-(
-    id            SERIAL PRIMARY KEY,
-    payload       json        not null,
-    status        varchar(15) not null default 'UBEHANDLET',
-    VERSJON       bigint               DEFAULT 0,
-    OPPRETTET_TID TIMESTAMP(3)         DEFAULT localtimestamp
+CREATE TABLE MOTTAK (
+  id SERIAL PRIMARY KEY
 );
-
-create index ON HENVENDELSE (status);
-
-CREATE TABLE HENVENDELSE_LOGG
-(
-    id             SERIAL PRIMARY KEY,
-    henvendelse_id bigint REFERENCES HENVENDELSE NOT NULL,
-    type           varchar(15)                   not null,
-    node           varchar(100)                  not null,
-    OPPRETTET_TID  TIMESTAMP(3) DEFAULT localtimestamp
-);
-
-create index ON HENVENDELSE_LOGG (henvendelse_id);
