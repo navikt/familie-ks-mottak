5279943980d853e9b52aac72443c1507e85b0da7
Martine Enger <marenger@student.matnat.uio.no>
2019-09-25 14:19:54 +0200

    Fjern unødvendig json-annotasjon på vedleggdto

--- src/main/java/no/nav/familie/ks/mottak/app/mottak/VedleggDto.java
+++ src/main/java/no/nav/familie/ks/mottak/app/mottak/VedleggDto.java
@@ -1,9 +1,7 @@
 package no.nav.familie.ks.mottak.app.mottak;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class VedleggDto {
 
     @JsonProperty
