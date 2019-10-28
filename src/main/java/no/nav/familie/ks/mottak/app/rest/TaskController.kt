b200b6efc613e880aaafcafc7b1a4286cf916ba0
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-28 09:11:51 +0100

    Legger til oidcutil bibliotek slik at vi får navident til bruker som gjør noe i systemet.

--- src/main/java/no/nav/familie/ks/mottak/app/rest/TaskController.kt
+++ src/main/java/no/nav/familie/ks/mottak/app/rest/TaskController.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ks.mottak.app.rest
 
 import no.nav.familie.ks.kontrakter.sak.Ressurs
 import no.nav.familie.prosessering.domene.Status
+import no.nav.familie.sikkerhet.OIDCUtil
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -10,32 +11,33 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api")
 @ProtectedWithClaims(issuer = "azuread")
 class TaskController(
-        private val restTaskService: RestTaskService) {
+        private val restTaskService: RestTaskService,
+        private val oidcUtil: OIDCUtil) {
 
     @GetMapping(path = ["/task"])
     fun task(@RequestHeader status: Status): ResponseEntity<Ressurs> {
-        val saksbehandlerId = "TODO"
+        val saksbehandlerId = oidcUtil.navIdent
 
         return ResponseEntity.ok(restTaskService.hentTasks(status, saksbehandlerId))
     }
 
     @PutMapping(path = ["/task/rekjor"])
     fun rekjørTask(@RequestParam taskId: Long): ResponseEntity<Ressurs> {
-        val saksbehandlerId = "TODO"
+        val saksbehandlerId = oidcUtil.navIdent
 
         return ResponseEntity.ok(restTaskService.rekjørTask(taskId, saksbehandlerId))
     }
 
     @PutMapping(path = ["task/rekjørAlle"])
     fun rekjørTasks(@RequestHeader status: Status): ResponseEntity<Ressurs> {
-        val saksbehandlerId = "TODO"
+        val saksbehandlerId = oidcUtil.navIdent
 
         return ResponseEntity.ok(restTaskService.rekjørTasks(status, saksbehandlerId))
     }
 
     @PutMapping(path = ["/task/avvikshaandter"])
     fun avvikshåndterTask(@RequestParam taskId: Long, @RequestBody avvikshåndterDTO: AvvikshåndterDTO): ResponseEntity<Ressurs> {
-        val saksbehandlerId = "TODO"
+        val saksbehandlerId = oidcUtil.navIdent
 
         return ResponseEntity.ok(restTaskService.avvikshåndterTask(taskId, avvikshåndterDTO.avvikstype, avvikshåndterDTO.årsak, saksbehandlerId))
     }
