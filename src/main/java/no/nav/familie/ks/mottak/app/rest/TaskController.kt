5fa13ed2237fde972445950563553ae7e25a9de9
Henning Solberg <henning.solberg@nav.no>
2019-10-29 22:39:23 +0100

    Ryddet i import

--- src/main/java/no/nav/familie/ks/mottak/app/rest/TaskController.kt
+++ src/main/java/no/nav/familie/ks/mottak/app/rest/TaskController.kt
@@ -4,9 +4,6 @@ import no.nav.familie.ks.kontrakter.sak.Ressurs
 import no.nav.familie.sikkerhet.OIDCUtil
 import no.nav.familie.prosessering.domene.Status
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
