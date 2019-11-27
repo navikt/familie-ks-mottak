ccdc5576debc829cc82b84f74b5a5d915b376cff
Geirmund Simonsen <geirmund.simonsen@nav.no>
2019-11-27 11:40:02 +0100

    Lagt til e2e-profil - skrur av FlywayMigrationConfig for profiler som ikke er prod / preprod

--- src/main/java/no/nav/familie/ks/mottak/config/FlywayMigrationConfig.java
+++ src/main/java/no/nav/familie/ks/mottak/config/FlywayMigrationConfig.java
@@ -1,13 +1,14 @@
 package no.nav.familie.ks.mottak.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("preprod | prod")
 @ConditionalOnProperty("spring.flyway.enabled")
 public class FlywayMigrationConfig {
 
