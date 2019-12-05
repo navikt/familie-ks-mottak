748a3b3fefa5fdafee97560281fedcd1a9fa47a7
Geirmund Simonsen <geirmund.simonsen@nav.no>
2019-12-05 13:05:31 +0100

    endringer i flywayconfig

--- src/main/java/no/nav/familie/ks/mottak/config/FlywayMigrationConfig.java
+++ src/main/java/no/nav/familie/ks/mottak/config/FlywayMigrationConfig.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("preprod | prod")
 public class FlywayMigrationConfig {
 
     @Bean
