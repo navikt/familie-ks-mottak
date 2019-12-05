f70bbf86cb3c0d60ed5888bbc7ef65db4d5c35d8
Geirmund Simonsen <geirmund.simonsen@nav.no>
2019-12-05 13:07:19 +0100

    flywayconfig endret

--- src/main/java/no/nav/familie/ks/mottak/config/FlywayMigrationConfig.java
+++ src/main/java/no/nav/familie/ks/mottak/config/FlywayMigrationConfig.java
@@ -1,11 +1,14 @@
 package no.nav.familie.ks.mottak.config;
 
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty("spring.flyway.enabled")
 public class FlywayMigrationConfig {
 
     @Bean
