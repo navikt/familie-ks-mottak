34f5368ec7369fb0a98bffade689b476a32a861b
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-06-29 16:17:23 +0200

    Ny versjon av felles og tatt i bruk triggertid ved feil

--- src/main/java/no/nav/familie/ks/mottak/app/task/HentSaksnummerFraJoarkTask.java
+++ src/main/java/no/nav/familie/ks/mottak/app/task/HentSaksnummerFraJoarkTask.java
@@ -16,8 +16,12 @@ import java.time.LocalDateTime;
 
 
 @Service
-@TaskStepBeskrivelse(taskStepType = HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK, maxAntallFeil = 100, beskrivelse = "Hent saksnummer fra joark")
+@TaskStepBeskrivelse(taskStepType = HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK,
+                     maxAntallFeil = 100,
+                     beskrivelse = "Hent saksnummer fra joark",
+                     triggerTidVedFeilISekunder = 60 * 15)
 public class HentSaksnummerFraJoarkTask implements AsyncTaskStep {
+
     private static final Logger LOG = LoggerFactory.getLogger(HentSaksnummerFraJoarkTask.class);
 
     public static final String HENT_SAKSNUMMER_FRA_JOARK = "hentSaksnummerFraJoark";
@@ -33,16 +37,9 @@ public class HentSaksnummerFraJoarkTask implements AsyncTaskStep {
 
     @Override
     public void doTask(Task task) {
-        try {
-            String saksnummer = hentJournalpostService.hentSaksnummer(task.getPayload());
-            task.getMetadata().put("saksnummer", saksnummer);
-            taskRepository.saveAndFlush(task);
-        } catch (HttpClientErrorException.NotFound notFound) {
-            LOG.info("Hent saksnummer returnerte 404 responsebody={}", notFound.getResponseBodyAsString());
-            task.setTriggerTid(LocalDateTime.now().plusMinutes(15));
-            taskRepository.save(task);
-            throw notFound;
-        }
+        String saksnummer = hentJournalpostService.hentSaksnummer(task.getPayload());
+        task.getMetadata().put("saksnummer", saksnummer);
+        taskRepository.saveAndFlush(task);
     }
 
     @Override
