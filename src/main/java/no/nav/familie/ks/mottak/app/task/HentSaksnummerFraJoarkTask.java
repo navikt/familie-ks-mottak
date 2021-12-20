a52240a2c83d96133eb51b013b265303da4c1494
Henning Ivan Solberg <hensol@users.noreply.github.com>
2021-12-20 12:59:40 +0100

    avvikshåndterer beslutningsstøtte hvis vi aldri finner noe i joark (#300)
    * avvikshåndterer beslutningsstøtte hvis vi aldri finner noe i joark

--- src/main/java/no/nav/familie/ks/mottak/app/task/HentSaksnummerFraJoarkTask.java
+++ src/main/java/no/nav/familie/ks/mottak/app/task/HentSaksnummerFraJoarkTask.java
@@ -4,9 +4,12 @@ import no.nav.familie.ks.mottak.app.mottak.HentJournalpostService;
 import no.nav.familie.ks.mottak.app.util.TaskUtil;
 import no.nav.familie.prosessering.AsyncTaskStep;
 import no.nav.familie.prosessering.TaskStepBeskrivelse;
+import no.nav.familie.prosessering.domene.Avvikstype;
+import no.nav.familie.prosessering.domene.Loggtype;
 import no.nav.familie.prosessering.domene.Task;
 import no.nav.familie.prosessering.domene.TaskRepository;
 import no.nav.familie.prosessering.internal.RekjørSenereException;
+
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,11 +22,12 @@ import java.time.LocalDateTime;
 
 @Service
 @TaskStepBeskrivelse(taskStepType = HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK,
-                     maxAntallFeil = 200,
+                     maxAntallFeil = HentSaksnummerFraJoarkTask.MAX_ANTALL_FEIL,
                      beskrivelse = "Hent saksnummer fra joark",
                      triggerTidVedFeilISekunder = 60 * 15)
 public class HentSaksnummerFraJoarkTask implements AsyncTaskStep {
 
+    public static final int MAX_ANTALL_FEIL = 200;
     public static final String HENT_SAKSNUMMER_FRA_JOARK = "hentSaksnummerFraJoark";
     private static final Logger LOG = LoggerFactory.getLogger(HentSaksnummerFraJoarkTask.class);
     private final TaskRepository taskRepository;
@@ -38,6 +42,15 @@ public class HentSaksnummerFraJoarkTask implements AsyncTaskStep {
 
     @Override
     public void doTask(Task task) {
+
+        long antallFeilendeForsøk = task.getLogg().stream().filter(t -> t.getType() == Loggtype.FEILET ).count();
+
+        if(antallFeilendeForsøk >= MAX_ANTALL_FEIL - 2) { // -2 for å unngå alarmer når max forsøk er nådd
+            task.avvikshåndter(Avvikstype.ANNET,"Oppdaterer ikke oppgave med beslutningsstøtte", "VL");
+            taskRepository.saveAndFlush(task);
+            return;
+        }
+
         try {
             String saksnummer = hentJournalpostService.hentSaksnummer(task.getPayload());
             task.getMetadata().put("saksnummer", saksnummer);
